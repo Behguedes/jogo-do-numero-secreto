0f9ea105fa9e9b8c5af36b1932089ee8aca80b2b
Brenda Nascimento <162237685+Behguedes@users.noreply.github.com>
2025-02-17 17:10:57 -0300

    commit

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
-//let titulo = document.querySelector('h1');
-//titulo.innerHTML = 'Jogo do número secreto';
-
-//let paragrafo = document.querySelector('p');
-//paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
-
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jogo do número secreto')
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10')
+exibirMensagemIniciar();
+
+function exibirMensagemIniciar() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
@@ -23,6 +21,7 @@ function verificarChute() {
         exibirTextoNaTela('h1', 'Acertou!');
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
         if (chute > numeroSecreto) {
             exibirTextoNaTela('p', 'O número secreto é menor!');
@@ -30,9 +29,23 @@ function verificarChute() {
             exibirTextoNaTela('p', 'O número secreto é maior!');
         }
         tentativas++;
+        limparCampo();
     }
 }
 
 function gerarNumeroAleatorio() {
    return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemIniciar();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
 }
